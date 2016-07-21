bf45af623812e0f1fd9654266cba3c49b157e33a
WildDima <dtopornin@gmail.com>
2016-07-21 18:39:16 +0300

    spec mocks

--- spec/controllers/fc_api/api/v1/cards_controller_spec.rb
+++ spec/controllers/fc_api/api/v1/cards_controller_spec.rb
@@ -5,11 +5,11 @@ module FcApi
     routes { FcApi::Engine.routes }
     # let(:card) { FactoryGirl.create(:card, user: user) }
     let :user do
-      user = double('User')
+      user = object_double('User')
       allow(user).to receive(:authentication_token).and_throw(SecureRandom.urlsafe_base64(nil, false))
     end
 
-    before { allow(Api::V1::CardsController).to receive(:current_user).and_return(user) }
+    before { allow_any_instance_of(Api::V1::CardsController).to receive(:current_user).and_return(user) }
 
     describe 'GET #index' do
       it 'returns http success' do
